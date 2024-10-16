70b379afa369a51122a93464c71c6a864ba4324f
sga1d0s <galdos.sergio@outlook.com>
2024-10-16 19:13:46 +0200

    [main] first commit

---
+++ Ejercicio3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+  public static void main(String[] args){
+
+    // 1.- Mensaje de bienvenida
+    System.out.println();
+    System.out.println("Caracter Creation");
+    System.out.println("-------------------");
+
+    // 2.- Read imputs
+    Scanner sc = new Scanner(System.in);
+
+    // Read name
+    String name = sc.next();
+
+  
+  }
+}
